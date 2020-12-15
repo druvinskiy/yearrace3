bc86de386ec9f8772a8f73ff0109f2228488e9f5
DavidRuvinskiy <david.ruvinskiy@sbcglobal.net>
2020-12-14 23:07:27 -0600

    Initial Commit

---
+++ yearrace3/yearrace3App.swift
@@ -0,0 +1,17 @@
+//
+//  yearrace3App.swift
+//  yearrace3
+//
+//  Created by David Ruvinskiy on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct yearrace3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
